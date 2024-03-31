b3d04f8cf16308b69b7ea2323e2148e13c60ed60
martoAs <martinaasteasuain22@gmail.com>
2024-03-31 12:17:14 -0300

    leap year agregado

--- app/src/main/java/ayds/songinfo/home/view/SongDescriptionHelperImpl.kt
+++ app/src/main/java/ayds/songinfo/home/view/SongDescriptionHelperImpl.kt
@@ -4,6 +4,7 @@ import ayds.songinfo.home.model.entities.Song
 import ayds.songinfo.home.model.entities.Song.EmptySong
 import ayds.songinfo.home.model.entities.Song.SpotifySong
 import kotlinx.datetime.Month
+
 interface SongDescriptionHelper {
     fun getSongDescriptionText(song: Song = EmptySong): String
 }
@@ -25,18 +26,26 @@ internal class SongDescriptionHelperImpl : SongDescriptionHelper {
 
     private fun releaseDateFormatter(song:SpotifySong):String{
         val listaFecha = song.releaseDate.split("-")
-        var formattedDate = ""
-        when(song.releaseDatePrecision){
-            "year" -> formattedDate = listaFecha[0] /* TODO: agregar leap year */
+        //var formattedDate = ""
+        return when(song.releaseDatePrecision){
+            "year" -> {
+                val year = listaFecha[0].toInt()
+                if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+                    "$year (leap year)"
+                else
+                    "$year (not a leap year)"
+
+            }
             "month" -> {
             var mes = Month(listaFecha[1].toInt()).name
             mes = mes.lowercase()
             mes = mes.replaceFirstChar { char -> char.uppercase() }
-            formattedDate = mes + ", " + listaFecha[0]
+            mes + ", " + listaFecha[0]
             }
-            "day" -> formattedDate = listaFecha[2] + "/" + listaFecha[1] + "/" + listaFecha[0]
+            "day" -> listaFecha[2] + "/" + listaFecha[1] + "/" + listaFecha[0]
+            else -> {song.releaseDate}
         }
-        return formattedDate
+
     }
 
 
