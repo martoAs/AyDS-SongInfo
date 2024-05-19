dcfa45f804ab6c8f62e2dbf34ed7178e323519b8
martoAs <martinaasteasuain22@gmail.com>
2024-05-19 17:04:23 -0300

    test de helper

--- app/src/test/java/ayds/songinfo/moredetails/presentation/ArticleBiographyHelperImplTest.kt
+++ app/src/test/java/ayds/songinfo/moredetails/presentation/ArticleBiographyHelperImplTest.kt
@@ -1,5 +1,23 @@
 package ayds.songinfo.moredetails.presentation
 
-import org.junit.jupiter.api.Assertions.*
+import ayds.songinfo.moredetails.domain.Article
 
-class ArticleBiographyHelperImplTest
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class ArticleBiographyHelperImplTest(){
+
+    private val articleBiographyHelper: ArticleBiographyHelper = ArticleBiographyHelperImpl()
+    private val footer = "</font></div></html>"
+    private val header = "<html><div width=400><font face=\"arial\">"
+    @Test
+    fun getDescriptionTestWithNoNewLines(){
+        val article = Article("Red Hot Chilli Peppers", "Hello, this is a description of the Red Hot Chilli Peppers. They are the best", "infoUrl")
+        val expectedDescription = "${header}Hello, this is a description of the <b>RED HOT CHILLI PEPPERS</b>. They are the best${footer}"
+
+        val resultDescription = articleBiographyHelper.getDescription(article)
+
+        assertEquals(expectedDescription, resultDescription)
+
+    }
+}
