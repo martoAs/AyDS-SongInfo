fa0fde93e4437536d63e6e04d86e21ae54f77699
martoAs <martinaasteasuain22@gmail.com>
2024-05-19 17:28:08 -0300

    mini cambio

--- app/src/test/java/ayds/songinfo/moredetails/presentation/ArticleBiographyHelperImplTest.kt
+++ app/src/test/java/ayds/songinfo/moredetails/presentation/ArticleBiographyHelperImplTest.kt
@@ -5,15 +5,17 @@ import ayds.songinfo.moredetails.domain.Article
 import org.junit.Test
 import org.junit.Assert.assertEquals
 
-class ArticleBiographyHelperImplTest(){
+class ArticleBiographyHelperImplTest {
 
     private val articleBiographyHelper: ArticleBiographyHelper = ArticleBiographyHelperImpl()
     private val footer = "</font></div></html>"
     private val header = "<html><div width=400><font face=\"arial\">"
     @Test
-    fun getDescriptionTestWithNoNewLines(){
-        val article = Article("Red Hot Chilli Peppers", "Hello, this is a description of the Red Hot Chilli Peppers. They are the best", "infoUrl")
-        val expectedDescription = "${header}Hello, this is a description of the <b>RED HOT CHILLI PEPPERS</b>. They are the best${footer}"
+    fun getDescriptionTest(){
+        val articleText = "Hello, this is a description of the Red Hot Chilli Peppers. They're the best.\n " +
+                "Psychic spies from China try to steal your mind's elation."
+        val article = Article("Red Hot Chilli Peppers", articleText, "infoUrl")
+        val expectedDescription = "${header}Hello, this is a description of the <b>RED HOT CHILLI PEPPERS</b>. They re the best.<br> Psychic spies from China try to steal your mind s elation.${footer}"
 
         val resultDescription = articleBiographyHelper.getDescription(article)
 
